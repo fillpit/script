db485a0bcfd45ab255d9593afc907768fbcbc203
fillpit <90119466+fillpit@users.noreply.github.com>
2024-05-21 15:37:32 +0800

    Create ros-custom-direct-ip.sh

---
+++ ros-custom-direct-ip.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+mkdir -p ./pbr
+cd ./pbr
+
+# AS4809 BGP
+wget --no-check-certificate -c -O custom-direct-ip.txt https://raw.githubusercontent.com/fillpit/script/main/clash/rules/custom-direct-ip.txt
+
+{
+echo "/ip firewall address-list"
+
+for net in $(cat custom-direct-ip.txt) ; do
+  if [[ $net != "#"* ]]
+  then
+      echo "add list=CN address=$net comment=custom ip"
+  fi
+  
+done
+
+} > ../RouterOS/custom-direct-ip.rsc
+
+cd ..
+rm -rf ./pbr
