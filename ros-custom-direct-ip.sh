621b5a6daf1ac027345de1c9f7f63db5f42596e9
fillpit <90119466+fillpit@users.noreply.github.com>
2024-05-21 16:04:18 +0800

    Update ros-custom-direct-ip.sh

--- ros-custom-direct-ip.sh
+++ ros-custom-direct-ip.sh
@@ -8,12 +8,11 @@ wget --no-check-certificate -c -O custom-direct-ip.txt https://raw.githubusercon
 {
 echo "/ip firewall address-list"
 
+ignore = "#"
 for net in $(cat custom-direct-ip.txt) ; do
-  if [[ $net != "#"* ]]
-  then
+  if [[ $net != *$ignore* ]]; then
       echo "add list=CN address=$net comment=custom ip"
   fi
-  
 done
 
 } > ../RouterOS/custom-direct-ip.rsc
