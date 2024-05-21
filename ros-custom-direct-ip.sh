d65d5a48a8fa6818298b78eab89cbdd3eb03e2b9
fillpit <90119466+fillpit@users.noreply.github.com>
2024-05-21 16:08:40 +0800

    Update ros-custom-direct-ip.sh

--- ros-custom-direct-ip.sh
+++ ros-custom-direct-ip.sh
@@ -5,12 +5,13 @@ cd ./pbr
 # AS4809 BGP
 wget --no-check-certificate -c -O custom-direct-ip.txt https://raw.githubusercontent.com/fillpit/script/main/clash/rules/custom-direct-ip.txt
 
+ignore = "#"
+
 {
 echo "/ip firewall address-list"
 
-ignore = "#"
 for net in $(cat custom-direct-ip.txt) ; do
-  if [[ $net != *$ignore* ]]; then
+  if [ $net != *$ignore* ]; then
       echo "add list=CN address=$net comment=custom ip"
   fi
 done
