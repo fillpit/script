11b824f7df8908ffb03eac8eccb913fb74a70386
fillpit <90119466+fillpit@users.noreply.github.com>
2024-05-25 09:25:01 +0800

    Update ros-custom-proxy-ip.sh

--- ros-custom-proxy-ip.sh
+++ ros-custom-proxy-ip.sh
@@ -10,15 +10,13 @@ wget --no-check-certificate -c -O custom-proxy-ip.txt https://raw.githubusercont
 echo "/ip firewall address-list"
 
 # 读取 custom-proxy-ip.txt 文件
-while read line; do
-    # 如果行不以 # 开头
-    if [[ ! -n "${line}" ]] && [[ "${line}" != "#"* ]]; then
+while read -r line; do
+    # 如果行不为空且不以 # 开头
+    if [[ -n "${line}" ]] && [[ "${line}" != "#"* ]]; then
         # 输出该行
         echo "add list=DO_PROXY_IP address=${line} comment=do_proxy_ip"
     fi
 done < custom-proxy-ip.txt
-
-} > ../RouterOS/custom-proxy-ip.rsc
 
 cd ..
 rm -rf ./pbr
